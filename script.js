f6f5f7f2640924b3ddbd6235ecb9709cff481c15
smokedev1 <154081467+smokedevv@users.noreply.github.com>
2024-12-06 13:15:44 +0200

    Update script.js

--- script.js
+++ script.js
@@ -22,3 +22,17 @@ function changeLanguage(lang) {
     document.getElementById('main-title').innerText = titles[lang].mainTitle;
     document.getElementById('post-heading').innerText = titles[lang].postHeading;
 }
+
+// Mouse movement event for the web effect
+document.addEventListener('mousemove', (event) => {
+    let spiderWeb = document.querySelector('.spider-web');
+    
+    if (!spiderWeb) {
+        spiderWeb = document.createElement('div');
+        spiderWeb.classList.add('spider-web');
+        document.body.appendChild(spiderWeb);
+    }
+    
+    spiderWeb.style.left = `${event.pageX - 200}px`;
+    spiderWeb.style.top = `${event.pageY - 200}px`;
+});
